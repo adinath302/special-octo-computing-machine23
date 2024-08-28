7841e95220a05cbdda7183a11556b9efab97bca2
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-08-28 22:36:39 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Navbar() {
+  return (
+    <div className="h-[60px] gap-2 bg-white my-4 rounded-lg flex justify-center items-center text-xl font-medium">
+      <img src="/logos_firebase.svg" alt="" />
+      <h1>Firebase contact app</h1>
+    </div>
+  );
+}
+
+export default Navbar;
